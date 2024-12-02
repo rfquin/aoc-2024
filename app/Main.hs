f10ccf4d70a567ccb5fb9aa8c028db48512b7828
Ben Quin <rfquinny@gmail.com>
2024-12-02 21:28:05 +1100

    Day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Day1 (day01)
+import Day2 (day02)
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -8,4 +9,5 @@ main = do
   args <- getArgs
   case args of
     "01": _ -> day01
+    "02": _ -> day02
     _ -> error "Sigma sigma on the wall"
